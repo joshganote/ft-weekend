8c9bc24b398677920e7467d3c68c9d28bdd3ca1b
joshganote <j.ganote0825@gmail.com>
2021-09-11 05:03:28 -0500

    docker setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello');
+  res.send('Hello!');
 });
 
 app.listen(5000, () => console.log('Server Running'));
