bf38663edecb463d3d5adddaa1d5ed6e154d69ee
Josh Ganote <j.ganote0825@gmail.com>
2021-09-12 00:25:44 -0500

    sucessful console log response from server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
+const cors = require("cors");
+app.use(cors({
+  origin: "http://localhost:3000",
+}))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello!');
+  res.send({ name: "josh"});
 });
 
 app.listen(5000, () => console.log('Server Running'));
